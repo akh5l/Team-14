182f53a35200d5ce73f8c678ead0b73ef60c1272
akh5l <xdteddypro@gmail.com>
2025-11-27 18:00:00 +0000

    Edit app template so favicon displays correctly

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bridge 14 Games</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     @vite('resources/css/app.css')
 </head>
 <body class="flex flex-col h-full">
