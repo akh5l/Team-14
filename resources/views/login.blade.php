43b12da6ef698f5dbe1c37bd8cc8bc81f8e3beac
YS-CS <myousafb112@gmail.com>
2025-11-27 17:33:21 +0000

    Making the cart page and the current tabs to link.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Bridge 14 Games Login</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100 font-sans min-h-screen flex flex-col">
+    <header 
+        <div class="">
+            // Testing, remove later, gonna do the login in page later
+        </div>
+        <nav class="">
+
+        </nav>
+    </header>
+
+</body>
