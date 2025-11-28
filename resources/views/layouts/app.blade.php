d6dd23da30574b2a7d428c50f7709fe6c9b8f4c2
akh5l <xdteddypro@gmail.com>
2025-11-28 13:22:34 +0000

    Add new background and blurred background to homepage heading text for legibility

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bridge 14 Games</title>
+    <title id="logo-text">Bridge 14 Games</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="preload" href="/images/background.webp" as="image">
     @vite('resources/css/app.css')
 </head>
 
@@ -15,7 +16,7 @@
         <a href="{{ url('/') }}"
             class="flex items-center gap-2 text-white font-bold text-xl sm:text-2xl variable-heading">
             <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-12 sm:h-15 w-auto">
-            Bridge 14 Games
+            <p id="logo-text">Bridge 14 Games</p>
         </a>
         <nav class="space-x-4 text-white font-semibold flex">
             <div class="flex items-center space-x-2 bg-white/20 rounded-xl px-3 py-2">
