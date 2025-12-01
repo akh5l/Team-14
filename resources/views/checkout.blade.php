bf785cf5a353229b65762284b6085fd55cef264c
YS-CS <myousafb112@gmail.com>
2025-12-01 19:38:09 +0000

    Checkout made base

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,38 +1,179 @@
 @extends('layouts.app')
 @section('content')
     <!--Tailwind Cheat Sheet: https://nerdcave.com/tailwind-cheat-sheet*-->
-    <section class="max-w-6xl mx-auto px-4 py-8 flex-grow">
-        <h1 class = "text-3xl font-bold mb-6">Checkout</h1>
-        <div class="md:grid md:grid-cols-3 gap-6 items-start">
-            <div class="bg-white rounded-lg shadow p-4 md:p-6 md:col-span-1 border border-gray-100">
-                <div class="flex flex-col md:flex-row gap-4">
+    <main class="max-w-6xl mx-auto px-4 py-12 flex-grow">
+        <!-- example and insp from this website https://www.convertcart.com/blog/checkout-page-examples-->
+        <h1 class="text-3x1 font-bold-mb-8">Checkout</h1>
+        <div class="md:grid md:grid-cols-3 gap-10">
 
-                    <div class="bg-white rounded-lg shadow p-4 md:p-6 md:col-span-1 border border-gray-100">
-                        <div class="flex flex-col md:flex-row gap-4">
-                            <!-- the example image and the propterties of it too -->
-                            <div class="flex gap-4">
-                                <img src="/images/products/d&d.png" alt="Product Image"
-                                    class="w-24 h-28 object-contain rounded bg-gray-100">
-                                <div>
-                                    <p class="font-semibold text-xl"> D&D Dragons of Stormwreck Isle Starter Set</p>
-                                    <p class="text-gray-600 text-sm"> Product #: 12345</p>
-                                    <div class="mt-3 flex items-center gap-2">
-                                        <span class="text-sm">Quantity:</span>
-                                        <input id="quantity" type="number" min="1" value="1"
-                                            class="w-16 border border-gray-300 rounded px-2 py-1 text-center text-sm">
-                                    </div>
-                                </div>
+            <!-- left side of the form with the payment form-->
+            <div class="md:col-span-2 bg-white p-6 rounded-lg shadow">
+                <!-- contact stuff and info -->
+                <h2 class="text-xl font-semibold mb-4">Contact Information</h2>
+                <form id ="checkoutForm" class="space-y-6">
 
-                                <!-- pricing and removing item button -->
-                                <div class="text-right">
-                                    <p id="line-total" class="font-semibold text-lg">£19.99</p>
-                                    <button class="text-xs text-blue-600 hover:underline mt-1">Remove</button>
-                                </div>
-                            </div>
-                        </div>
+                    <!-- Name form -->
+                    <div>
+                        <label class="block text-sm font-medium mb-1">Full Name</label>
+                        <input id="fullName"
+                                 type="text"
+                                 required
+                                 placeholder="John Pork"
+                                 class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-blue-500 focus:outline-none">
                     </div>
-                </div>
+                    
+                    <!-- Email form -->
+                    <div>
+                        <label class="block text-sm font-medium mb-1">Email Address</label>
+                        <input id="email"
+                                 type ="email"
+                                 placeholder="example@gmail.com"
+                                 required
+                                 class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-blue-500 focus:outline-none">
+                    </div>
+                    <hr class = "my-6"> <!--faint line below-->
+
+                    <!--Card details-->
+                    <div>
+                        <label class="block text-sm font-medium mb-1">Card Details</label>
+                        <input id ="cardNumber"
+                                 type="text"
+                                 required
+                                 inputemode ="numeric"
+                                 placeholder= "1234 5678 9012 3456"
+                                 class="w-full border border-gray-300 rounded-lg px-4 py-2 tracking-widest focus:ring-blue-500 focus:outline-none">
+                    </div>
+
+                    <!-- Expiration of CVC -->
+                    <div class="flex gap-4">
+                        <div class="flex-1">
+                            <label class="block text-sm font-medium mb-1">Expiration Date</label>
+                            <input id="expiry"
+                                    type="month"
+                                    required
+                                    class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-blue-500 focus:outline-none">
+                        </div>
+                    <!--CVV data-->
+                    <div class="w-28">
+                        <label class="block text-sm font-medium mb-1">CVV</label>
+                        <input id= "cvv"
+                                type="text"
+                                maxlength="3"
+                                pattern="[0-9]{3}"
+                                placeholder="123"
+                                required
+                                inputmode="numeric"
+                                class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-blue-500 focus:outline-none">
+                    </div> 
+
+            </div>
+            <!-- name onm the card-->
+            <div>
+                <label class="block text-sm font-medium mb-1">Name on Card</label>
+                <input id="cardName"
+                        type="text"
+                        required
+                        placeholder="Exactly as on your card!"
+                        class= "w-full border border-gray-300 rounded-lg px-4 py-2 focus:rind-blue-500 focus:outline-none">
+            </div>
+
+            <!-- error message for wrong input -->
+             <p id="errorBox" class="text-red-600 text-sm font-medium hidden"></p>
+            <!--Button-->
+            <button type="submit"
+                    class="w-full bg-red-600 hover:bg-red-700 text-white px-2 rouded-lg font-semibold">
+                Place order
+            </button>
+
+        </form>
+    </div>
+
+    <!-- right side for summary of product(s)-->
+    <div class ="md:grid md:cols-span-3 gap-10">
+        <div class="bg-white p-6 rounded-lg shadow border border-gray-100 h-fit">
+            <h2 class="text-xl font-semibold mb-4">Order Summary</h2>
+        
+        <div class="flex items-center gap-4 mb-4">
+            <img src ="/images/products/d&d.png"
+                class="w-20 h-20 rounded object-contains bg-gray-100" alt="D&D Dragons of Stormwreck Isle">
+            <div class="flex-1">
+                <p class="font-semibold">D&D Dragons of Stormwreck Isle</p>
+                <p class="text-sm text-gray-500">Qauntity: 1</p>
+            </div>
+            <p class="font-semibold text-sm">£19.99</p>
+        </div>
+        <hr class= "my-4">
+
+        <div class="text-sm space-y-2">
+            <div class="flex justify-between">
+                <span>Subtotal:</span>
+                <span>£19.99</span>
+            </div>
+            <div class="flex justify-between">
+                <span>Delivery:</span>
+                <span>£0.00</span>
+            </div>
+            <div class="flex justify-between font-semibold text-lg pt-2">
+                <span>Total:</span>
+                <span>£19.99</span>
             </div>
         </div>
-    </section>
+    </div>
+
+</div>
+
+</main>
+
+<!-- scripts for the validation rules etc.-->
+ <script>
+document.getElementById("checkoutForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const name = document.getElementById("fullName").value.trim();
+    const cardName= document.getElementById("cardName").value.trim();
+    const cardNumber = document.getElementById("cardNumber").value;
+    const cvv =document.getElementById("cvv").value;
+    const expiry = document.getElementById("expiry").value;
+    const errorBox =document.getElementById("errorBox");
+
+//ALL CARD VALIDATIONS:
+    if (name.toLowerCase() !== cardName.toLowerCase()) {
+        errorBox.textContent= "The name on the card must be the same as the one entered above!";
+        errorBox.classList.remove("hidden");
+        return;
+    }
+    const rawCardNumber = document.getElementById("cardNumber").value.replace(/\s/g, "");
+    if (!/^[0-9]{16}$/.test(rawCardNumber)) {
+        errorBox.textContent = "Card number must consist of 16 digits!"
+        errorBox.classList.remove("hidden");
+        return;
+    }
+
+    const today = new Date();
+    const check = new Date(expiry + "-01T00:00:00");
+    if (check <= today) { //has to be future - to make sure usable card
+        errorBox.textContent = "This card has expired!";
+        errorBox.classList.remove("hidden");
+        return;
+    }
+
+//Otehr form validations
+    errorBox.classList.add("hidden");
+    window.location.href = "/order/complete";
+});
+
+//https://syntaxsimplified.com/cheatsheet/Javascript/javascript.html some help from this site too
+</script>
+
+<script>
+const cardInput = document.getElementById("cardNumber");
+cardInput.addEventListener("input", function(e) {
+    let value = e.target.value.replace(/\D/g, " ");
+
+    value = value.substring(0, 16);
+    const groups = value.match(/.{1,4}/g); 
+    e.target.value = groups ? groups.join(" ") : "";
+
+});
+</script>
+
 @endsection
