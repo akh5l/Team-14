7972b6838d5ef36ddecd884098ead1eaa6a64dd5
Creengu <terencemorgan39@gmail.com>
2025-12-05 10:09:58 +0000

    added some makeshift product descriptions

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -43,16 +43,35 @@
             <div>
                 <h3 class="text-2xl font-semibold mb-4">Product Details</h3>
                 <p class="mb-4">
-                    {{ $product->detailed_description ?? 'PLACEHOLDER add a detailed description field to each product, using Braden\'s trello descriptions!!!' }}
+                    {{ $product->description }}
                 </p>
 
                 <ul class="list-disc list-inside mb-4">
-                    <li>PLACEHOLDER Make this list specific to each product?</li>
                     <li>Durable materials</li>
                     <!-- If a product is above £40, add a section under 'product details' stating that is eligible for free delivery -->
                     @if($product->price > 40)
                         <li>Eligible for free delivery</li>
                     @endif
+                    @if ($product->product_type == 'consoles' )
+                        <li>Includes all necessary cables and controllers</li>
+                        <li>games sold separately</li>
+
+                    @elseif ($product->product_type == 'tabletop_games' )
+                        <li>Includes all items and instructions necessary to begin your journey</li>
+                        <li>Suitable for ages 13 and up</li>
+                        
+                    @elseif ($product->product_type == 'video_games_accessories')
+                        <li>1-year warranty included</li>
+
+                        @elseif ($product->product_type == 'video_games')
+                        <li>Available on multiple platforms</li>
+                        <li>Features engaging and immersive gameplay</li>
+
+                        @elseif ($product->product_type == 'tabletop_accessories')
+                        <li>Perfect for enhancing your gameplay experience </li>
+                        <li>Perfect for hobbyists and casual gamers alike</li>
+                    @endif
+                    
                 </ul>
             </div>
 
