268586b0d910750b17e3a98ae6d736fed387cb28
akh5l <xdteddypro@gmail.com>
2025-12-05 09:55:35 +0000

    Add random customer names shown in product reviews on product details page

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -55,11 +55,14 @@
                 </p>
 
                 <ul class="list-disc list-inside mb-4">
-                    <li>PLACEHOLDER Make this list specific to each product?</li>
+                    {{-- <li>PLACEHOLDER Make this list specific to each product?</li> --}}
+
                     <li>Durable materials</li>
+                    <li>Made using environmentally sustainable practises</li>
+                    
                     <!-- If a product is above £40, add a section under 'product details' stating that is eligible for free delivery -->
                     @if ($product->price > 40)
-                        <li>Eligible for free delivery</li>
+                        <li>Eligible for free delivery - over £40</li>
                     @endif
                 </ul>
             </div>
@@ -67,15 +70,18 @@
             <div>
                 <h3 class="text-2xl font-semibold mb-4">Customer Reviews</h3>
                 <div class="space-y-4">
+                    @php
+                        $customers = \App\Models\User::where('role', '!=', 'admin')->inRandomOrder()->take(2)->get();
+                    @endphp
                     <div class="bg-gray-100 p-4 rounded-lg shadow">
                         <p class="mb-2">"Great product; highly recommend"</p>
-                        <div class="text-sm text-gray-600">- customer 1 PLACEHOLDER add dummy reviews from customers later
+                        <div class="text-sm text-gray-600">- {{ $customers[0]->first_name . " " . $customers[0]->last_name ?? 'Anonymous' }}
                         </div>
                     </div>
 
                     <div class="bg-gray-100 p-4 rounded-lg shadow">
                         <p class="mb-2">"Good quality and fast shipping"</p>
-                        <div class="text-sm text-gray-600">- customer 2</div>
+                        <div class="text-sm text-gray-600">- {{ $customers[1]->first_name . " " . $customers[1]->last_name ?? 'Anonymous' }}</div>
                     </div>
                 </div>
             </div>
