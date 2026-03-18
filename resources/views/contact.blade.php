a20ea1341248a5ab462c0739d8f5a5f7794ed46e
xNavss <navundeep2006@gmail.com>
2026-03-18 23:31:57 +0000

    Changed colour scheme for Contact Us webpage

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,15 +6,15 @@
 
         <img src="/images/contact_question.png" class="w-[800px] h-auto object-contain">
 
-        <div class="w-full max-w-xl bg-white shadow-md rounded-xl p-8">
+        <div class="w-full max-w-xl bg-[#2c2f66] shadow-2xl rounded-2xl p-8 text-white">
 
-            <h2 class="text-3xl font-bold mb-2 text-gray-700">Contact Us</h2>
-            <p class="text-gray-500 mb-6 text-sm">
+            <h2 class="text-3xl font-bold mb-2 text-white">Contact Us</h2>
+            <p class="text-gray-300 mb-6 text-sm">
                 We aim to respond promptly to all enquiries. Watch out for an email!
             </p>
 
             <!-- Contact information -->
-            <div class="mb-8 text-gray-600 text-sm space-y-2">
+            <div class="mb-8 text-gray-200 text-sm space-y-2">
                 <p class="flex items-center gap-2">
                     📍 <span>45 Gamer's Avenue, Birmingham, UK</span>
                 </p>
@@ -30,7 +30,7 @@
             </div>
             
             <!-- Small map of where area is based -->
-            <div class="mb-6">
+            <div class="p-[2px] rounded-xl bg-[#6163b3]">
                 <iframe
                     width="100%"
                     height="180"
@@ -45,7 +45,7 @@
 
             <!-- Success message when user submits the contact form -->
             @if(request()->has('success'))
-                <div class="mb-4 bg-green-100 text-green-700 text-sm px-4 py-2 rounded">
+                <div class="mb-4 bg-[#85c061]/20 text-[#85c061] text-sm px-4 py-2 rounded">
                     Your message has been sent successfully.
                 </div>
             @endif
@@ -53,7 +53,7 @@
             <form action="https://api.web3forms.com/submit" method="POST" class="space-y-5">
 
                 <!-- This is required for Web3Forms to work, easier way for forms to be sent to us. -->
-                <input
+                <input 
                     type="hidden"
                     name="access_key"
                     value="71e04a53-b0ce-4775-9570-e8ec09d93a3c"
@@ -67,7 +67,7 @@
                 >
 
                 <!-- Input field for Name, presence check is implemented. -->
-                <input
+                <input class="w-full bg-[#292755] border border-[#6163b3] text-white rounded-lg px-4 py-2 focus:ring-2 focus:ring-[#6163b3] focus:outline-none"
                     type="text"
                     name="name"
                     class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
@@ -76,7 +76,7 @@
                 >
 
                 <!-- Input field for Email, presence check and format check is implemented. -->
-                <input
+                <input class="w-full bg-[#292755] border border-[#6163b3] text-white rounded-lg px-4 py-2 focus:ring-2 focus:ring-[#6163b3] focus:outline-none"
                     type="email"
                     name="email"
                     class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
@@ -86,7 +86,7 @@
 
                 <!-- Input field for Message, presence check and length check is implemented. -->
                 <!-- Minimum Characters: 50 | Maximum Characters: 500 -->
-                <textarea
+                <textarea class="w-full bg-[#292755] border border-[#6163b3] text-white rounded-lg px-4 py-2 focus:ring-2 focus:ring-[#6163b3] focus:outline-none"
                     name="message"
                     rows="6"
                     minlength="50"
@@ -99,7 +99,7 @@
                 <!-- Submit button for submitting the data. -->
                 <button
                     type="submit"
-                    class="w-full bg-blue-500 text-white font-semibold px-4 py-2 rounded-lg hover:bg-blue-800 transition"
+                    class="w-full bg-[#6163b3] hover:bg-[#4c4fa3] text-white font-semibold px-4 py-2 rounded-lg hover:bg-blue-800 transition"
                 >
                     Send Message
                 </button>
