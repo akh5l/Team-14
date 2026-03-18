9292a926f809f0788bf7841b224aae05170e1b52
akh5l <xdteddypro@gmail.com>
2026-03-18 01:38:48 +0000

    Add game-specific details i.e., platform, developer, age rating

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -31,6 +31,48 @@ class ProductController extends Controller
     public function show($id)
     {
         $product = Product::with('category')->findOrFail($id);
+
+        // game-specific details (not worth adding to DB at this stage, but possible improvement)
+
+        if ($product->category_id == 2) {
+
+            $gameDetails = [
+                'God of War: Ragnarök'                    => [
+                    'platform'   => 'PS5',
+                    'developer'  => 'Santa Monica Studio',
+                    'age_rating' => 'PEGI 18',
+                ],
+                'Red Dead Redemption 2'                   => [
+                    'platform'   => 'PS4, PS5',
+                    'developer'  => 'Rockstar Games',
+                    'age_rating' => 'PEGI 18',
+                ],
+                'Cyberpunk 2077'                          => [
+                    'platform'   => 'PS4, PS5',
+                    'developer'  => 'CD Projekt Red',
+                    'age_rating' => 'PEGI 18',
+                ],
+                'The Legend of Zelda: Breath of the Wild' => [
+                    'platform'   => 'Nintendo Switch, Nintendo Switch 2',
+                    'developer'  => 'Nintendo EPD',
+                    'age_rating' => 'PEGI 12',
+                ],
+                'Elden Ring'                              => [
+                    'platform'   => 'PS5',
+                    'developer'  => 'FromSoftware',
+                    'age_rating' => 'PEGI 16',
+                ],
+            ];
+
+            // attach details if the product is in mapping
+            if (array_key_exists($product->product_name, $gameDetails)) {
+                foreach ($gameDetails[$product->product_name] as $key => $value) {
+                    $product->$key = $value;
+                }
+            }
+
+        }
+
         return view('products.show', compact('product'));
     }
 }
