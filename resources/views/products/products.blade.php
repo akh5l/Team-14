013faf866f981775393a1e73334b1f2c9c837409
xNavss <navundeep2006@gmail.com>
2026-03-20 14:38:47 +0000

    Header in Products colour changed to appropriate appearance

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="max-w-7xl mx-auto px-4 py-12">
         
-        <h1 class="text-3xl font-bold text-center mb-8">
+        <h1 class="text-black dark:text-white text-3xl font-bold text-center mb-8">
             {{ $currentCategory->category_name ?? 'All Products' }}
         </h1>
 
