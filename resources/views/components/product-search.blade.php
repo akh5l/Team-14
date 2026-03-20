12ce5c678c09e41c217fc3bf1d5669321bfb4ff9
Raj Sukhnain <rsukhnain2006@gmail.com>
2026-03-20 17:21:32 +0000

    added light mode colour to category buttons

--- resources/views/components/product-search.blade.php
+++ resources/views/components/product-search.blade.php
@@ -32,13 +32,13 @@ new class extends Component {
 
     <div class="flex flex-wrap justify-center gap-4 mb-8">
         <button wire:click="$set('search', '')"
-            class="dark:bg-[#6163b3] dark:hover:bg-[#4c4fa3] dark:text-white px-4 py-2 rounded transition">
+            class="bg-[#9d8cd9] hover:bg-[#8b75d7] dark:bg-[#6163b3] dark:hover:bg-[#4c4fa3] dark:text-white px-4 py-2 rounded transition">
             All Products
         </button>
 
         @foreach ($this->categories as $category)
             <button wire:click="$set('search', '{{ addslashes($category->category_name) }}')"
-                class="dark:bg-[#6163b3] dark:hover:bg-[#4c4fa3] dark:text-white px-4 py-2 rounded transition">
+                class="bg-[#9d8cd9] hover:bg-[#8b75d7] dark:bg-[#6163b3] dark:hover:bg-[#4c4fa3] dark:text-white px-4 py-2 rounded transition">
                 {{ $category->category_name }}
             </button>
         @endforeach
