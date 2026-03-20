dc37ace62e4060c0b2d48295594b696889be5660
xNavss <navundeep2006@gmail.com>
2026-03-20 15:24:49 +0000

    Remade light and dark mode when selecting a product. Made light mode for reviews section

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -9,8 +9,8 @@
         </div>
 
         <div class="md:w-1/2 flex flex-col justify-center">
-            <h1 class="text-4xl font-bold mb-4">{{ $product->product_name }}</h1>
-            <p class="text-lg mb-4">{{ $product->description }}</p>
+            <h1 class="text-4xl font-bold mb-4 text-black dark:text-white">{{ $product->product_name }}</h1>
+            <p class="text-lg mb-4 text-gray-800 dark:text-gray-200">{{ $product->description }}</p>
 
             <p class="text-2xl font-semibold mb-6 text-red-500">
                 £{{ number_format($product->price, 2) }}
@@ -27,14 +27,14 @@
 
                 <form method="POST" action="{{ route('cart.buyNow', $product) }}">
                     @csrf
-                    <button type="submit" class="bg-blue-500 text-white py-3 px-6 rounded-lg hover:bg-blue-600 transition font-semibold">
+                    <button type="submit" class="bg-[#9d8cd9] hover:bg-[#8677bc] dark:bg-[#6163b3] dark:hover:bg-[#515399] text-white py-3 px-6 rounded-lg transition font-semibold">
                         Buy Now
                     </button>
                 </form>
                 @endauth
 
                 @guest
-                <a href="/login" class="bg-blue-500 text-white py-3 px-6 rounded-lg hover:bg-blue-600 transition font-semibold">
+                <a href="/login" class="bg-[#9d8cd9] hover:bg-[#8879bf] dark:bg-[#6163b3] dark:hover:bg-[#505193] text-white py-3 px-6 rounded-lg transition font-semibold">
                     Sign in to buy
                 </a>
                 @endguest
@@ -46,19 +46,19 @@
 </section>
 
 <section class="max-w-7xl mx-auto px-4 py-12">
-    <h2 class="text-3xl font-bold mb-8 text-center">Product Details & Reviews</h2>
+    <h2 class="text-3xl text-black dark:text-white font-bold mb-8 text-center">Product Details & Reviews</h2>
 
     <div class="grid md:grid-cols-2 gap-8">
 
         <div>
-            <h3 class="text-2xl font-semibold mb-4">Product Details</h3>
+            <h3 class="text-2xl text-black dark:text-white font-semibold mb-4">Product Details</h3>
             {{-- showing description again is redundant here --}}
 
             {{-- <p class="mb-4">
                     {{ $product->description }}
             </p> --}}
 
-            <ul class="list-disc list-inside mb-4">
+            <ul class="list-disc list-inside mb-4 text-gray-800 dark:text-gray-300">
 
                 @if ($product->category_id == 1)
                 <li>Includes all items and instructions necessary to begin your journey</li>
@@ -88,16 +88,16 @@
         </div>
 
         <div>
-            <h3 class="text-2xl font-semibold mb-4">Customer Reviews</h3>
+            <h3 class="text-2xl font-semibold mb-4 dark:text-white">Customer Reviews</h3>
 
             @auth
             <form method="POST" action="{{ route('reviews.store', $product->product_id) }}">
                 @csrf
 
-                <textarea name="comment" class="w-full bg-[#292755] border border-[#6163b3] text-white placeholder-gray-200 rounded-lg px-4 py-2 mb-3" placeholder="Write your review..." required></textarea>
+                <textarea name="comment" class="w-full bg-[#e5e0f7] dark:bg-[#292755] border border-[#6163b3] text-white palceholder-gray-800 dark:placeholder-gray-200 rounded-lg px-4 py-2 mb-3" placeholder="Write your review..." required></textarea>
 
                 <div class="flex flex-col sm:flex-row gap-3 mb-8">
-                    <select name="rating" class="w-1/2 bg-[#292755] border border-[#6163b3] text-white rounded-lg px-4 py-2" required>
+                    <select name="rating" class="w-1/2 bg-[#e5e0f7] dark:bg-[#292755] border dark:border-[#6163b3] dark:text-white rounded-lg px-4 py-2" required>
                         <option value="">Rating</option>
                         <option value="5">⭐⭐⭐⭐⭐</option>
                         <option value="4">⭐⭐⭐⭐</option>
@@ -106,7 +106,7 @@
                         <option value="1">⭐</option>
                     </select>
 
-                    <button class="bg-[#6163b3] w-1/2 px-4 py-2 rounded-lg text-white">
+                    <button class="bg-[#9d8cd9] hover:bg-[#8879bf] dark:bg-[#6163b3] dark:hover:bg-[#505193] w-1/2 px-4 py-2 rounded-lg text-white">
                         Submit Review
                     </button>
                 </div>
@@ -133,7 +133,7 @@
                     </div>
                 </div>
                 @empty
-                <p class="text-gray-800">No reviews yet.</p>
+                <p class="text-gray-800 dark:text-gray-200">No reviews yet.</p>
                 @endforelse
 
             </div>
