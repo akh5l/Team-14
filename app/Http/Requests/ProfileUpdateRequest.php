13813433f6107bfa27f68a9b037a13c39e8c6804
akh5l <xdteddypro@gmail.com>
2026-03-20 13:46:38 +0000

    Change frontend, ProfileController and ProfileUpdateRequest to allow users to change their email address

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -15,16 +15,13 @@ class ProfileUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'lowercase',
-                'email',
-                'max:255',
-                Rule::unique(User::class)->ignore($this->user()->id),
-            ],
-        ];
+    return [
+        'current_email' => ['required', 'email'],
+        'email' => [
+            'required',
+            'email',
+            Rule::unique('users')->ignore($this->user()->user_id, 'user_id'),
+        ],
+    ];
     }
 }
