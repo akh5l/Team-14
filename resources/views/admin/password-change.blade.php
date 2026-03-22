ff787870873445dd76bc48da764856ad162a6290
akh5l <xdteddypro@gmail.com>
2026-03-22 13:35:18 +0000

    Fix missing dark mode on admin password change page

--- resources/views/admin/password-change.blade.php
+++ resources/views/admin/password-change.blade.php
@@ -4,11 +4,11 @@
 <main class="max-w-6xl mx-auto px-4 py-12">
 
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Update Password') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-300">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
     </header>
@@ -18,14 +18,14 @@
         @method('put')
 
         <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <x-input-label for="update_password_password" :value="__('New Password')" class="dark:text-gray-100"/>
+            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full dark:text-gray-800" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
         <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')"  class="dark:text-gray-100"/>
+            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full dark:text-gray-800" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
