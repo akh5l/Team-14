0c5b3442f84b515b4c430a63e6579667062fb16b
akh5l <xdteddypro@gmail.com>
2026-03-22 11:48:56 +0000

    Fix missing import in OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\Product;
 use App\Models\StockLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
